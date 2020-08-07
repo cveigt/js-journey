de3f041d082d2755716cefe6f8a410dca13f8154
Christian Veigt <cveigt@gmail.com>
2020-08-07 11:11:43 -0700

    Add continue and break statements

--- script.js
+++ script.js
@@ -574,7 +574,6 @@ for (var i = 1; i <= 20; i++) {
 for (var i = 1; i <= 20; i += 2) {
     console.log(i);
 }
-*/
 
 //LOOPS
 
@@ -587,4 +586,23 @@ var i = 0;
 while(i < john,length) {
     console.log(john[i]);
     i++;
+}
+*/
+
+//continue and break statements
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+    if(typeof john[i] != 'string') continue;
+    console.log(john[i]);
+}
+
+for (var i = 0; i < john.length; i++) {
+    if(typeof john[i] != 'string') break;
+    console.log(john[i]);
+}
+
+//looping backwards
+for(var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
 }
