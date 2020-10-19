4881b78cedd724ea21ea55debfe1da0ad185877f
cveigt <cveigt@gmail.com>
2020-10-19 01:26:13 -0700

    finish first part

--- DOM-pig-game/starter/app.js
+++ DOM-pig-game/starter/app.js
@@ -15,15 +15,12 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     
@@ -32,7 +29,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
     //2. Display the result
     var diceDOM = document.querySelector('.dice');
-    dice.DOM.style.display = 'block';
+    diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
 
