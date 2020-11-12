e44a2122b6b3cb571f9b9aa5a235ab2043f391d7
cveigt <cveigt@gmail.com>
2020-11-12 00:22:53 -0800

    Add function expression and arrow functions

--- 02/script.js
+++ 02/script.js
@@ -13,3 +13,27 @@ const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
 */
 
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    //return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'John'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
