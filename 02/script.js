9d2f478f07c2902a7c70d086578227f26546b9f5
cveigt <cveigt@gmail.com>
2020-11-11 23:44:57 -0800

    Add functions code

---
+++ 02/script.js
@@ -0,0 +1,15 @@
+/*'use strict';
+
+let hasDriverLicense = false;
+const passTest = true;
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+*/
+
