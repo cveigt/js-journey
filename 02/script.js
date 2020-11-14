fa9505e201ec1ea5c4a93e0038f1b4448cdadc4d
cveigt <cveigt@gmail.com>
2020-11-13 23:49:16 -0800

    Add Arrays

--- 02/script.js
+++ 02/script.js
@@ -11,7 +11,6 @@ function fruitProcessor(apples, oranges) {
 
 const appleJuice = fruitProcessor(5, 0);
 console.log(appleJuice);
-*/
 
 function calcAge1(birthYear) {
     return 2037 - birthYear;
@@ -37,3 +36,106 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'John'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+};
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    
+    const juice = `juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+};
+
+console.log(fruitProcessor(2,3));
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);        
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retires.`);
+        return -1;
+    }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+const friends = ['Mike', 'Steve', 'Pete'];
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+console.log(y[0]);
+
+const firstName = 'jonas';
+const jonas = [firstName, 'Tran', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+//add elements
+const newLength = friends.push('Jay')
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+//remove elements
+
+friends.pop(); //remove last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if(friends.includes('Steven')) {
+    console.log('Steven is your friend');
+} else {
+    console.log('Not a friend')
+}
