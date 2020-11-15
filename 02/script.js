85469484c35cd195a50561aa907c6dfe467af2a0
cveigt <cveigt@gmail.com>
2020-11-15 14:22:58 -0800

    Add looping arrays

--- 02/script.js
+++ 02/script.js
@@ -215,8 +215,62 @@ console.log(jonas.age);
 //console.log(jonas['calcAge'](jonas.birthYear));
 
 console.log(jonas.getSummary());
-*/
 
 for(let rep = 1; rep <= 30; rep++) {
     console.log(`lifting rep ${rep}`);
+    
 }
+const jonas = [
+    'Jonas',
+    'Doe',
+    2037 - 1991,
+    'teacher',
+    ['Mike', 'Peter', 'Steve'],
+    true
+];
+
+const types = [];
+
+for(let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof jonas[i]);
+
+    //types[i] = typeof jonas[i];
+    types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for(let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+//continue and break
+console.log('--- Only strings ---')
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] !== 'string') continue;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- Break with number ---')
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] === 'number') break;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+const jonas = [
+    'Jonas',
+    'Doe',
+    2037 - 1991,
+    'teacher',
+    ['Mike', 'Peter', 'Steve'],
+    true
+];
+
+for(let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+*/
