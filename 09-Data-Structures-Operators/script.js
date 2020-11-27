0201dbfe8860ceed8079b46e3262447600f55f63
cveigt <cveigt@gmail.com>
2020-11-27 01:17:49 -0800

    Finish destructuring  objects

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -25,7 +25,29 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${[this.starterMenu[starterIndex]]} and ${
+        this.mainMenu[mainIndex]
+      } will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: '1212 king street',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({ address: '1212 king street', starterIndex: 1 });
 
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
