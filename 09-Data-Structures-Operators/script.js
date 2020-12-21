3b5e92a9b0a964cc35ce173d9696be24433a764f
cveigt <cveigt@gmail.com>
2020-12-20 16:11:01 -0800

    Object keys, values, and entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -48,11 +48,27 @@ const restaurant = {
   },
 };
 
+// Property NAMES
 const properties = Object.keys(openingHours);
 console.log(properties);
 
+let openStr = `We are open on ${[properties.length]} days:`;
 for (const day of Object.keys(openingHours)) {
-  console.log(day);
+  openStr += `${day},`;
+}
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+//console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
 
 /*
