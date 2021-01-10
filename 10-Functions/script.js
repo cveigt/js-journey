78fd6a6912e7dc29d736fb29fdc1854261a591fe
cveigt <cveigt@gmail.com>
2021-01-09 23:54:37 -0800

    Add passing arguments

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,31 @@
 "use strict";
 
+const flight = "LH234";
+const jonas = {
+  name: "Jonas S",
+  passport: 1234567890,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 1234567890) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Is the same as doing
+const flightNum = flight;
+const passport = jonas;
+
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -24,3 +50,4 @@ createBooking("LH123", 2, 800);
 createBooking("LH123", 2);
 createBooking("LH123", 5);
 createBooking("LH123", undefined, 1000);
+*/
