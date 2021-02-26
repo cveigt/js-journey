1de094c3d9389380bae6edd43b882dbbd57c91a3
cveigt <cveigt@gmail.com>
2021-02-25 17:28:29 -0800

    Add Callback function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,4 +1,22 @@
-"use strict";
+'use strict';
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
 
 /*
 const flight = "LH234";
