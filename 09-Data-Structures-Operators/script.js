acdd45bfc193f2a26b1e8d5cf3c98b446883f35d
cveigt <cveigt@gmail.com>
2021-03-23 00:40:03 -0700

    Add Maps

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -48,6 +48,77 @@ const restaurant = {
   },
 };
 
+////////////////////////////////////////////////
+// Maps: Iteration
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'correct :)'],
+  [false, 'try again :('],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+// for (const options of question) console.log(options);
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/*
+////////////////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firense, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+console.log(
+  rest
+    .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'we are open :D')
+    .set(false, 'we are closed :(')
+);
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
+
+////////////////////////////////////////////////////////
+// Sets
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -80,6 +151,7 @@ console.log(new Set('Jonasschmedtmann').size);
 console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'][0]).size
 );
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #2
