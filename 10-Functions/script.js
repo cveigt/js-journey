a9102eb8c5f08afab12e7f8294c5e7d99f7bf217
cveigt <cveigt@gmail.com>
2021-04-09 17:26:46 -0700

    Add Functions accepting callback functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋🏻');
+};
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -33,7 +62,7 @@ newPassport(jonas);
 checkIn(flight, jonas);
 console.log(jonas);
 
-/*
+
 const bookings = [];
 
 const createBooking = function (
