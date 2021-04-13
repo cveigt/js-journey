f3ed10db45bd03828d1825ef35c4b23a7ba1d150
cveigt <cveigt@gmail.com>
2021-04-12 17:35:48 -0700

    coding challenge 10.1

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,49 @@
 'use strict';
 
+const poll = {
+  question: 'What is your favorite programming language?',
+  options: ['0: JavaScript', '1: Phyton', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0].
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // Register answers
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(',')}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
+
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -95,7 +139,6 @@ const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
 
-/*
 const greet = function (greeting) {
   return function (name) {
     console.log(`${greeting} ${name}`);
