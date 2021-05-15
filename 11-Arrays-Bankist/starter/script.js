47a7f6c42d176b6336f3d846ff641c603d375fe8
cveigt <cveigt@gmail.com>
2021-05-14 21:32:02 -0700

    nit(added for loop equivalent for Reduce method)

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -225,3 +225,7 @@ console.log(movements);
 // }, 0);
 const balance = movements.reduce((acc, cur) => acc + cur, 0);
 console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
